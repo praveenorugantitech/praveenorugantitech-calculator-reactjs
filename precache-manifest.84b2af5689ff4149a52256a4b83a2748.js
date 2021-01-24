75b9b2f5311d13a26faf5734d4aef1e522fe3e2b
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-24 22:55:59 +0530

    Updates

---
+++ precache-manifest.84b2af5689ff4149a52256a4b83a2748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b54e554d65f1c6273da7d206b72766",
+    "url": "/praveenorugantitech-calculator-reactjs/index.html"
+  },
+  {
+    "revision": "7a7828460f0bb47826c7",
+    "url": "/praveenorugantitech-calculator-reactjs/static/css/main.5cfc58c3.chunk.css"
+  },
+  {
+    "revision": "7471a4fa5f71a242ed0b",
+    "url": "/praveenorugantitech-calculator-reactjs/static/js/2.961ab765.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/praveenorugantitech-calculator-reactjs/static/js/2.961ab765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7828460f0bb47826c7",
+    "url": "/praveenorugantitech-calculator-reactjs/static/js/main.d2370e1f.chunk.js"
+  },
+  {
+    "revision": "e496d100c91d0e1fb6e6",
+    "url": "/praveenorugantitech-calculator-reactjs/static/js/runtime-main.9956126d.js"
+  }
+]);
